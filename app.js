9c8dd99911ea4810d718f7cd64f020a7d58b880f
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-03-14 00:13:44 +0530

    crud complete

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const mongoose = require('mongoose');
 const seedDB = require('./seed');
 const productRoutes = require("./routes/productRoutes");
 const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override')
+
+ 
+
 
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://127.0.0.1:27017/shopping-sam-app')
+mongoose.connect('mongodb://127.0.0.1:27017/shopping-g2-app')
 .then(()=>{console.log("DB connected")})
 .catch((err)=>{console.log(err)})
  
@@ -18,6 +22,8 @@ app.set('views' , path.join(__dirname,'views'));
 // now for public folder
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({extended:true}));
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
 
 
 // seeding dummy data
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -29,5 +29,28 @@ router.get('/products/:id' , async(req,res)=>{
 
 })
 
+// form to edit a product
+router.get('/products/:id/edit', async(req,res)=>{
+    let {id} = req.params;
+    let foundProduct = await Product.findById(id);
+    res.render('products/edit' , {foundProduct})
+})
+
+
+// actually changing the product in db
+router.patch('/products/:id' , async (req,res)=>{
+    let {id} = req.params;
+    let {name , img, price , desc} = req.body;
+    await Product.findByIdAndUpdate(id ,  {name , img, price , desc});
+    res.redirect('/products');
+})
+
+//deleting a product
+router.delete('/products/:id' , async(req,res)=>{
+    let {id} = req.params;
+    await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+
+})
 
 module.exports = router;
