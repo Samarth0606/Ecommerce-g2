2e25e4b96dc66b79d923ad5cbefa18e1b362320d
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-03-22 15:55:07 +0530

    cookies and try catch

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,52 +4,101 @@ const router = express.Router();
 
 // displaying all the products
 router.get('/products' , async(req,res)=>{
-    let products = await Product.find({});
-    res.render('products/index' , {products});
+    try{
+
+        let products = await Product.find({});
+        res.render('products/index' , {products});
+
+    }
+    catch(e){
+        res.render('error' , {err: e.message} )
+    }
+    
 })
 
 
 // adding a fomr for  anew product
 router.get('/products/new' , (req,res)=>{
-    res.render('products/new');
+    try{
+
+        res.render('products/new');
+    }
+
+    catch(e){
+        res.render('error' , {err: e.message} )
+    }
 })
 
 // actually adding a product in a DB 
 router.post('/products' , async (req,res)=>{
-    let {name,img,price,desc} = req.body;
-    await Product.create({name,img,price,desc});
-    res.redirect('/products');
+    try{
+
+        let {name,img,price,desc} = req.body;
+        await Product.create({name,img,price,desc});
+        res.redirect('/products');
+    }
+
+    catch(e){
+        res.render('error' , {err: e.message} )
+    }
 })
 
 // route for shwoing the deatails of thre products
 router.get('/products/:id' , async(req,res)=>{
-    let {id} = req.params;
-    let foundProduct = await Product.findById(id).populate('reviews');
-    res.render('products/show' , {foundProduct});
+    try{
+
+        let {id} = req.params;
+        let foundProduct = await Product.findById(id).populate('reviews');
+        res.render('products/show' , {foundProduct});
+    }
+
+    catch(e){
+        res.render('error' , {err: e.message} )
+    }
 
 })
 
 // form to edit a product
 router.get('/products/:id/edit', async(req,res)=>{
-    let {id} = req.params;
-    let foundProduct = await Product.findById(id);
-    res.render('products/edit' , {foundProduct})
+    try{
+
+        let {id} = req.params;
+        let foundProduct = await Product.findById(id);
+        res.render('products/edit' , {foundProduct});
+    }
+
+    catch(e){
+        res.render('error' , {err: e.message} )
+    }
 })
 
 
 // actually changing the product in db
 router.patch('/products/:id' , async (req,res)=>{
-    let {id} = req.params;
-    let {name , img, price , desc} = req.body;
-    await Product.findByIdAndUpdate(id ,  {name , img, price , desc});
-    res.redirect('/products');
+    try{
+
+        let {id} = req.params;
+        let {name , img, price , desc} = req.body;
+        await Product.findByIdAndUpdate(id ,  {name , img, price , desc});
+        res.redirect('/products');
+    }
+
+    catch(e){
+        res.render('error' , {err: e.message} )
+    }
 })
 
 //deleting a product
 router.delete('/products/:id' , async(req,res)=>{
-    let {id} = req.params;
-    await Product.findByIdAndDelete(id);
-    res.redirect('/products');
+    try{
+        let {id} = req.params;
+        await Product.findByIdAndDelete(id);
+        res.redirect('/products');
+    }
+        
+    catch(e){
+        res.render('error' , {err: e.message} )
+    }
 
 })
 
--- routes/review.js
+++ routes/review.js
@@ -4,20 +4,27 @@ const Review = require('../models/review');
 const router = express.Router();
 
 router.post('/products/:productId/review' , async(req,res)=>{
-    // console.log(req.body);
-    let {productId} = req.params;
-    let {rating, comment} = req.body;
-    const review = new  Review({rating, comment});
-    const product = await Product.findById(productId);
-    product.reviews.push(review);
 
-    console.log(product);
-    console.log(review);
+    try{
+            // console.log(req.body);
+        let {productId} = req.params;
+        let {rating, comment} = req.body;
+        const review = new  Review({rating, comment});
+        const product = await Product.findById(productId);
+        product.reviews.push(review);
+
+        console.log(product);
+        console.log(review);
+        
+        await product.save()
+        await review.save()
+
+        res.redirect(`/products/${productId}`);
+    }
+    catch(e){
+        res.render('error' , {err: e.message} )
+    }
     
-    await product.save()
-    await review.save()
-
-    res.redirect(`/products/${productId}`);
 })
 
 
