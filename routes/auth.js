6a548cd564e6de5e7ae8939706c167c0a5a24af7
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-03-28 15:17:31 +0530

    auth done

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,57 @@
 const express = require('express');
-const User = require('../models/user');
 const router = express.Router();
+const User = require('../models/user');
+const passport = require('passport');
+// const passport = require('passport');
+// const { registerForm, registerNewUser, loginForm, loginUser,logoutUser } = require('../controllers/auth');
 
-router.get('/fakeuser' , async(req,res)=>{
+// router.get('/fakeuser', async(req, res) => {
+    
 
-    const user = {
-        email:'sam@gmail.com',
-        username:'sam'
-    }
+    // const user = {
+    //     email: 'samarth@gmail.com',
+    //     username:'samarth'
+    // }
+    // const newUser = await User.register(user, 'sam123');
 
-    await User.register(user,'sam123');
-    res.send(user);
+    // res.send(newUser);
+// });
 
+
+router.get('/register' , (req,res)=>{
+    res.render('auth/signup');
 })
 
+router.post('/register' , async(req,res)=>{
+    let {email,username,password} = req.body;
+    const user = new User({email,username});
+    const newUser = await User.register(user,password);
+    res.send(newUser);
+})
+
+router.get('/login' , (req,res)=>{
+    res.render('auth/login');
+})
+        
+    
+router.post('/login',
+    passport.authenticate('local', { 
+       failureRedirect: '/login', 
+       failureMessage: true ,
+    }),
+    function(req, res) {
+        // console.log(req.user);
+        req.flash('success' , `welcome back ${req.user.username}`)
+        res.redirect('/products');
+    }
+);
+
+router.get('/logout',(req,res)=>{
+    ()=>{
+        req.logout();
+    }
+    req.flash('success' , 'goodbye friend');
+    res.redirect('/products');
+});
 
 module.exports = router;
