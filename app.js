74acc212d1372c50bf73e73439bc5e10bd9f2256
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-03-28 15:13:12 +0530

    auth done

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport =  require('passport');
+const LocalStrategy =  require('passport-local');
+const User = require('./models/user');
+
 
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost:27017/shopping-g2-app')
@@ -29,6 +33,17 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+passport.use(new LocalStrategy(User.authenticate()));
+
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
--- models/user.js
+++ models/user.js
@@ -1,18 +1,57 @@
-const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const passport = require('passport');
+// const passport = require('passport');
+// const { registerForm, registerNewUser, loginForm, loginUser,logoutUser } = require('../controllers/auth');
+
+// router.get('/fakeuser', async(req, res) => {
+    
+
+    // const user = {
+    //     email: 'samarth@gmail.com',
+    //     username:'samarth'
+    // }
+    // const newUser = await User.register(user, 'sam123');
+
+    // res.send(newUser);
+// });
 
 
-let userSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        trim:true,
-        required:true
-    }
+router.get('/register' , (req,res)=>{
+    res.render('auth/signup');
 })
 
-userSchema.plugin(passportLocalMongoose);
+router.post('/register' , async(req,res)=>{
+    let {email,username,password} = req.body;
+    const user = new User({email,username});
+    const newUser = await User.register(user,password);
+    res.send(newUser);
+})
 
-let User = mongoose.model('User' , userSchema )
+router.get('/login' , (req,res)=>{
+    res.render('auth/login');
+})
+        
+    
+router.post('/login',
+    passport.authenticate('local', { 
+       failureRedirect: '/login', 
+       failureMessage: true ,
+    }),
+    function(req, res) {
+        // console.log(req.user);
+        req.flash('success' , `welcome back ${req.user.username}`)
+        res.redirect('/products');
+    }
+);
 
+router.get('/logout',(req,res)=>{
+    ()=>{
+        req.logout();
+    }
+    req.flash('success' , 'goodbye friend');
+    res.redirect('/products');
+});
 
-module.exports = User;
+module.exports = router;
